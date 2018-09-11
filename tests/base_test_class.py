d4a45a7fc80645e980b1563188b530081e26413a
Jeroen Dries <jeroen.dries@vito.be>
2018-09-11 11:49:30 +0200

    added a test for building a WMTS based on accumulo

---
+++ tests/base_test_class.py
@@ -0,0 +1,48 @@
+import unittest
+
+from pyspark import find_spark_home
+import os,sys
+from glob import glob
+
+spark_python = os.path.join(find_spark_home._find_spark_home(), 'python')
+py4j = glob(os.path.join(spark_python, 'lib', 'py4j-*.zip'))[0]
+sys.path[:0] = [spark_python, py4j]
+
+
+
+from geopyspark import geopyspark_conf, Pyramid, TiledRasterLayer
+from pyspark import SparkContext
+
+#this is a copy of the base class in the geopyspark driver
+class BaseTestClass(unittest.TestCase):
+    if 'TRAVIS' in os.environ:
+        master_str = "local[2]"
+    else:
+        master_str = "local[*]"
+
+    conf = geopyspark_conf(master=master_str, appName="test")
+    conf.set('spark.kryoserializer.buffer.max', value='1G')
+    conf.set('spark.ui.enabled', True)
+
+    if 'TRAVIS' in os.environ:
+        conf.set(key='spark.driver.memory', value='2G')
+        conf.set(key='spark.executor.memory', value='2G')
+
+    pysc = SparkContext.getOrCreate(conf)
+
+    #dir_path = geotiff_test_path("all-ones.tif")
+
+    # rdd = get(LayerType.SPATIAL, dir_path, max_tile_size=1024)
+    # value = rdd.to_numpy_rdd().collect()[0]
+    #
+    # projected_extent = value[0]
+    # extent = projected_extent.extent
+    #
+    # expected_tile = value[1].cells
+    # (_, rows, cols) = expected_tile.shape
+    #
+    # layout = TileLayout(1, 1, cols, rows)
+
+    @classmethod
+    def to_pyramid(cls,tiled_rdd:TiledRasterLayer):
+        return Pyramid( {0:tiled_rdd})
---
+++ tests/test_accumulo_viewing.py
@@ -0,0 +1,17 @@
+from base_test_class import BaseTestClass
+from unittest import TestCase,skip
+from openeo.rest import rest_session
+import requests
+import os
+from shapely.geometry import Polygon
+from openeogeotrellis.GeotrellisCatalogImageCollection import GeotrellisCatalogImageCollection
+
+
+class Test(BaseTestClass):
+
+    @skip
+    def test_wmts_from_accumulo(self):
+        imagecollection = GeotrellisCatalogImageCollection(image_collection_id="CGS_SENTINEL2_RADIOMETRY_V102")
+        service = imagecollection.tiled_viewing_service(type="WMTS")
+        print(service)
+        print(service['url'])
