c8389b59adef783ba7aa6d2f2b7b5a0411e90378
Jan Van den bosch <jan.van.den.bosch@devoteam.com>
2019-04-03 11:27:15 +0200

    updated submit.sh
    use Spark 2.3.1 to cope with JavaGateway issues
    fixes regarding dependencies and shared libraries

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,9 +1,12 @@
-#!/bin/sh -e
+#!/usr/bin/env bash
+
+set -eo pipefail
 
 jobName=$1
 
+export HDP_VERSION=3.0.0.0-1634
 export SPARK_MAJOR_VERSION=2
-export SPARK_HOME=/usr/hdp/current/spark2-client
+export SPARK_HOME=/usr/hdp/$HDP_VERSION/spark2/
 export PYSPARK_PYTHON="./python"
 export PATH="$SPARK_HOME/bin:$PATH"
 
@@ -18,20 +21,20 @@ hadoop fs -put -f venv.zip ${hdfsVenvDir}
 
 hdfsVenvZip=hdfs:/user/jenkins/${hdfsVenvDir}/venv.zip
 
-extensions=$(ls geotrellis-extensions-*.jar)
+extensions=$(ls openeo-geopyspark-driver/jars/geotrellis-extensions-*.jar)
 backend_assembly=$(find $VIRTUAL_ENV -name 'geotrellis-backend-assembly-*.jar')
 
 echo "Found backend assembly: ${backend_assembly}"
 
-appId=$(yarn application -list 2>&1 | grep ${jobName} | awk '{print $1}')
-if [ ! -z "$var" ]
+appId=$(yarn application -list 2>&1 | grep ${jobName} | awk '{print $1}' || true)
+if [ ! -z "$appId" ]
 then
     echo "Killing running intergration test service: ${appId}"
-    yarn application -kill ${appId} || true
+    yarn application -kill ${appId}
 fi
 
 echo "Submitting: ${jobName}"
-spark-submit \
+${SPARK_HOME}/bin/spark-submit \
  --master yarn --deploy-mode cluster \
  --principal jenkins@VGT.VITO.BE --keytab ${HOME}/jenkins.keytab \
  --driver-memory 4G \
@@ -41,10 +44,11 @@ spark-submit \
  --conf spark.dynamicAllocation.minExecutors=20 \
  --conf spark.locality.wait=300ms --conf spark.shuffle.service.enabled=true --conf spark.dynamicAllocation.enabled=true \
  --conf spark.yarn.submit.waitAppCompletion=false \
- --conf "spark.yarn.appMasterEnv.SPARK_HOME=$SPARK_HOME" --conf spark.yarn.appMasterEnv.PYTHON_EGG_CACHE=./ \
+ --conf spark.yarn.appMasterEnv.PYTHON_EGG_CACHE=./ \
  --conf "spark.yarn.appMasterEnv.PYSPARK_PYTHON=$PYSPARK_PYTHON" \
  --conf "spark.yarn.appMasterEnv.OPENEO_VENV_ZIP=$hdfsVenvZip" \
  --conf spark.executorEnv.DRIVER_IMPLEMENTATION_PACKAGE=openeogeotrellis --conf spark.yarn.appMasterEnv.DRIVER_IMPLEMENTATION_PACKAGE=openeogeotrellis \
+ --conf spark.executorEnv.LD_LIBRARY_PATH=/opt/rh/rh-python35/root/usr/lib64 --conf spark.yarn.appMasterEnv.LD_LIBRARY_PATH=/opt/rh/rh-python35/root/usr/lib64 \
  --files python,$(ls typing-*-none-any.whl),openeo-geopyspark-driver/layercatalog.json,openeo-geopyspark-driver/scripts/submit_batch_job.sh,openeo-geopyspark-driver/scripts/log4j.properties,openeo-geopyspark-driver/openeogeotrellis/deploy/batch_job.py \
  --archives "${hdfsVenvZip}#venv" \
  --conf spark.hadoop.security.authentication=kerberos --conf spark.yarn.maxAppAttempts=1 \
