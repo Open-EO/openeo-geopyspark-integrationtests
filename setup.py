389df504fab8edcf1aa73dcfd526a257f3e56137
Jeroen Dries <jeroen.dries@vito.be>
2019-07-03 16:25:23 +0200

    add pytest-runner

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='openeo-integration-tests',
     test_suite='tests',
+    setup_requires=['pytest-runner'],
     tests_require=['requests','openeo-udf','pytest'],
 )
