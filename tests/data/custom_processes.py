8add2c6dd4a486d85bc5512759056e8752f547c1
Jeroen Dries <jeroen.dries@vito.be>
2020-10-24 20:56:10 +0200

    fix integrationtest startup

--- tests/data/custom_processes.py
+++ tests/data/custom_processes.py
@@ -1,9 +1,10 @@
 from openeo_driver.ProcessGraphDeserializer import custom_process
 from openeo_driver.save_result import JSONResult
+from openeo_driver.utils import EvalEnv
 
 
 @custom_process
-def foobar(args: dict, viewingParameters: dict):
+def foobar(args: dict, env: EvalEnv = None):
     return JSONResult(data={
         "args": sorted(args.keys()),
         "msg": "hello world",
