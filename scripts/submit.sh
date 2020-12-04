58d4078d68264c1afa2178305d418d1986f6c059
Jeroen Dries <jeroen.dries@vito.be>
2020-12-04 07:51:16 +0100

    Dockerize integrationtest, spark 3.0, Python 3.8

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -8,14 +8,14 @@ pysparkPython="venv/bin/python"
 
 export HDP_VERSION=3.1.0.0-78
 export SPARK_MAJOR_VERSION=2
-export SPARK_HOME=/usr/hdp/$HDP_VERSION/spark2/
+export SPARK_HOME=/opt/spark3_0_0
 export PATH="$SPARK_HOME/bin:$PATH"
 
-export PYTHONPATH="venv/lib64/python3.6/site-packages:venv/lib/python3.6/site-packages"
+export PYTHONPATH="venv/lib64/python3.8/site-packages:venv/lib/python3.8/site-packages"
 
 hdfsVenvZip=https://artifactory.vgt.vito.be/auxdata-public/openeo/dev/openeo-"${version}".zip
-extensions=https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.0.0-SNAPSHOT/geotrellis-extensions-2.0.0-SNAPSHOT.jar
-backend_assembly=https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo.jar
+extensions=https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-extensions-2.0.0_2.12-SNAPSHOT.jar
+backend_assembly=https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo_2.12.jar
 
 echo "Found backend assembly: ${backend_assembly}"
 
@@ -24,8 +24,8 @@ date
 ${SPARK_HOME}/bin/spark-submit \
  --master yarn --deploy-mode cluster \
  --queue lowlatency \
- --principal jenkins@VGT.VITO.BE --keytab ${HOME}/jenkins.keytab \
  --driver-memory 2G \
+ --principal jenkins@VGT.VITO.BE --keytab ${HOME}/jenkins.keytab \
  --conf spark.executor.cores=2 \
  --driver-java-options "-Dlog4j.debug=true -Dlog4j.configuration=file:log4j.properties" \
  --conf spark.driver.memoryOverhead=3g \
@@ -45,6 +45,12 @@ ${SPARK_HOME}/bin/spark-submit \
  --conf spark.yarn.appMasterEnv.WMTS_BASE_URL_PATTERN=http://tsviewer-rest-test.vgt.vito.be/openeo/services/%s \
  --conf spark.executorEnv.AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --conf spark.yarn.appMasterEnv.AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
  --conf spark.executorEnv.AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} --conf spark.yarn.appMasterEnv.AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+ --conf spark.yarn.appMasterEnv.YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS=/var/lib/sss/pipes:/var/lib/sss/pipes:rw,/usr/hdp/current/:/usr/hdp/current/:ro,/etc/hadoop/conf/:/etc/hadoop/conf/:ro,/etc/krb5.conf:/etc/krb5.conf:ro,/data/MTDA:/data/MTDA:ro \
+ --conf spark.yarn.appMasterEnv.YARN_CONTAINER_RUNTIME_TYPE=docker \
+ --conf spark.yarn.appMasterEnv.YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=vito-docker-private-dev.artifactory.vgt.vito.be/python38-hadoop \
+ --conf spark.executorEnv.YARN_CONTAINER_RUNTIME_TYPE=docker \
+ --conf spark.executorEnv.YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=vito-docker-private-dev.artifactory.vgt.vito.be/python38-hadoop \
+ --conf spark.executorEnv.YARN_CONTAINER_RUNTIME_DOCKER_MOUNTS=/var/lib/sss/pipes:/var/lib/sss/pipes:rw,/usr/hdp/current/:/usr/hdp/current/:ro,/etc/hadoop/conf/:/etc/hadoop/conf/:ro,/etc/krb5.conf:/etc/krb5.conf:ro,/data/MTDA:/data/MTDA:ro \
  --files venv36/layercatalog.json,venv36/log4j.properties \
  --py-files tests/data/custom_processes.py \
  --archives "${hdfsVenvZip}#venv" \
