1585ab72c0845af8c28b55f3bb18e7be7348f017
Jeroen Dries <jeroen.dries@vito.be>
2020-12-08 20:27:44 +0100

    increase memory

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -28,7 +28,7 @@ ${SPARK_HOME}/bin/spark-submit \
  --principal jenkins@VGT.VITO.BE --keytab ${HOME}/jenkins.keytab \
  --conf spark.executor.cores=2 \
  --driver-java-options "-Dlog4j.debug=true -Dlog4j.configuration=file:log4j.properties -Dhdp.version=3.1.4.0-315" \
- --conf spark.driver.memoryOverhead=3g \
+ --conf spark.driver.memoryOverhead=4g \
  --conf spark.executor.memoryOverhead=512m \
  --conf spark.executor.memory=2G \
  --conf spark.speculation=true \
