0d61c45a5dc6df57555cae20f8dbaf4b7ac0081a
DRIESSEB <bart.driessen@vito.be>
2021-07-12 13:42:58 +0200

    EP-3918: Review feedback

--- tests/vegindex_calculator.py
+++ tests/vegindex_calculator.py
@@ -81,7 +81,7 @@ def compute_indices(datacube: DataCube, index_list: list) -> DataCube:
     ("NDVI", ["B04", "B08"], [[0.8563234, 0.845991], [0.82082057, 0.841846]]),
     ("NDMI", ["B08", "B11"], [[0.48937103, 0.4992478], [0.47535053, 0.50764006]]),
 ])
-def test_vegindex_calculator_ndvi(auth_connection: Connection, index, bands, expected):
+def test_vegindex_calculator_ndvi(auth_connection: Connection, tmp_path, index, bands, expected):
     x = 640860.000
     y = 5676170.000
     bbox = (x,y,x+20,y+20)
@@ -93,9 +93,9 @@ def test_vegindex_calculator_ndvi(auth_connection: Connection, index, bands, exp
 
     s2_masked = s2.process("mask_scl_dilation", data=s2, scl_band_name="SCL").filter_bands(bands)
     feats = compute_indices(s2_masked, [index])
-    feats.download("./tmp/feats.tif", format="GTiff")
+    feats.download(tmp_path / "feats.tif", format="GTiff")
 
-    with rasterio.open('./tmp/feats.tif') as dataset:
+    with rasterio.open(tmp_path / 'feats.tif') as dataset:
         result = dataset.read(3)
 
     np.testing.assert_almost_equal(actual=result, desired=expected, decimal=7)
