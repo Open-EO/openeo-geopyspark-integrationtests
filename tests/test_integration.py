061284780293e84a229f142955801b3a24b5735e
Stefaan Lippens <stefaan.lippens@vito.be>
2024-01-19 14:18:47 +0100

    fixup! Issue #13 update test_ndvi_band_math extent

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -292,7 +292,7 @@ def test_ndvi_band_math(auth_connection, tmp_path, api_version):
 
     out_file = tmp_path / "ndvi.tiff"
     ndvi.download(out_file, format="GTIFF")
-    assert_geotiff_basics(out_file, min_height=40, expected_shape=(1, 360, 344))
+    assert_geotiff_basics(out_file, min_height=40, expected_shape=(1, 344, 360))
     with rasterio.open(out_file) as ds:
         x = ds.read(1)
         assert np.nanmin(x, axis=None) == pytest.approx(0.0183, abs=0.01)
