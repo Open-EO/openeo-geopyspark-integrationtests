b152ad56a33b83d659cd494e8442f3330c5706c9
Jan Van den bosch <jan.van.den.bosch@devoteam.com>
2024-04-25 14:32:54 +0200

    add procedure for problems re: deleting batch jobs
    https://github.com/Open-EO/openeo-geopyspark-driver/issues/763

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -801,7 +801,10 @@ def test_batch_job_delete_job(auth_connection):
 
     try:
         resp = job.describe_job()
-        raise RuntimeError(f"Expected 404 Not Found, but got {resp}")
+        raise RuntimeError(f"Expected 404 Not Found, but still got a job back: {resp}\n\n"
+                           f'If this problem persists, make sure that the ES index used by the EJR API has a mapping '
+                           f'for the "deleted" property. The EJR CLI app should not be returning batch jobs for which '
+                           f'the "deleted" property is set.')
     except OpenEoApiError as e:
         assert e.http_status_code == 404
 
