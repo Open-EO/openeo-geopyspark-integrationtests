59925066ccc0ffed2d5c59405a101327525c7ff7
Stefaan Lippens <stefaan.lippens@vito.be>
2024-07-26 18:14:05 +0200

    setup.py: depend on vanilla openeo>=0.31.0 now it's released

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "schema",
         "pytest-timeout",
         "shapely",
-        "openeo>=0.31.0.a2.dev",  # TODO: drop ".a2.dev" suffix once openeo 0.31.0 is released
+        "openeo>=0.31.0",
         "openeo_driver>=0.39.1.dev",
         "pytest-xdist",
         "xarray>=2022.0.0",
