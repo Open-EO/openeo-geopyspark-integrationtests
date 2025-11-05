a4ff9b5601bfb88ffe4ae01f649463c57cdfe441
Jeroen Dries <jeroen.dries@vito.be>
2025-11-05 10:12:34 +0100

    udf signature fix

--- tests/data/udfs/raster_collections_ndvi.py
+++ tests/data/udfs/raster_collections_ndvi.py
@@ -1,12 +1,10 @@
 import xarray
 
-from openeo.udf import XarrayDataCube
 
-
-def apply_datacube(array: xarray.DataArray, context: dict) -> xarray.DataArray:
+def apply_datacube(cube: xarray.DataArray, context: dict) -> xarray.DataArray:
     """Compute the NDVI based on sentinel2 tiles"""
-    red = array.sel(bands="B04")
-    nir = array.sel(bands="B08")
+    red = cube.sel(bands="B04")
+    nir = cube.sel(bands="B08")
     ndvi = (nir - red) / (nir + red)
 
     return ndvi
